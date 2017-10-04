4496904b12568bbe81bf9046bb87097f359d2d22
Khan M Rashedun-Naby <naby88@gmail.com>
2017-10-05 00:44:45 +0600

    Some Minor Changes

--- src/AccountWidgetInit/AccountWidgetInit.php
+++ src/AccountWidgetInit/AccountWidgetInit.php
@@ -1,14 +1,26 @@
-<?php
+<?php # -*- coding: utf-8 -*-
 
 namespace TheDramatist\WooComAW\AccountWidgetInit;
 
+/**
+ * Class AccountWidgetInit
+ *
+ * @package TheDramatist\WooComAW\AccountWidgetInit
+ */
 class AccountWidgetInit {
 
+	/**
+	 * This method is responsible for initializing the widget.
+	 */
 	public function init() {
 		add_action( 'widgets_init', [ $this, 'register_widget' ] );
 	}
 
+	/**
+	 * Registering the widget.
+	 */
 	public function register_widget() {
 		return register_widget( '\TheDramatist\WooComAW\AccountWidgetCore\AccountWidgetCore' );
 	}
+
 }
